d6574629d31cc15630da0334273e08d1205f1b1b
xufanjie123 <527719396@qq.com>
2018-11-20 23:50:56 +0800

    Initial commit

---
+++ src/main/java/com/example/concuurrency/ConcuurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.example.concuurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcuurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcuurrencyApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/concuurrency/annoations/Demo.java
@@ -0,0 +1,13 @@
+package com.example.concuurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Demo {
+
+    String value() default "";
+}
